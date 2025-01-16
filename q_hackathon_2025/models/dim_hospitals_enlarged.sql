1a8ad8569d1546ab66fe11c89c35b0fa2b245a0b
Andrea Montes <amontes@qventus.com>
2025-01-16 15:52:54 -0500

    Enriched model

--- q_hackathon_2025/models/dim_hospitals_enlarged.sql
+++ q_hackathon_2025/models/dim_hospitals_enlarged.sql
@@ -1,12 +1,17 @@
+WITH enhanced_hospitals AS (
+    SELECT 
+        *,
+        TRIM(
+            REGEXP_REPLACE(
+                hospital_name, 
+                'Northwestern Medicine', 
+                ''
+            )
+        ) AS cleaned_hospital_name
+    FROM {{ ref("northwestern_enlarge_hospital_dataset") }}
+)
 SELECT 
-    * 
-FROM {{source("q_lake_hospitals", "main_hospitals")}} AS  mh
-LEFT JOIN {{ ref("northwestern_enlarge_hospital_dataset")}} AS ehd
-ON mh.name LIKE TRIM(
-        REGEXP_REPLACE(
-            ehd.hospital_name, 
-            '(?i)\bNorthwestern Medicine\b|\b', 
-            '', 
-            'g'
-        )
-    ) 
+    *
+FROM {{source("q_lake_hospitals", "main_hospitals")}} AS mh
+LEFT JOIN enhanced_hospitals AS ehd
+ON mh.name LIKE ehd.cleaned_hospital_name
